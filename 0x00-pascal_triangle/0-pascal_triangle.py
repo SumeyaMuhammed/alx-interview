815c15585af087dd76311531ee1db8ca8f9dcac7
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-03 09:23:41 -1100

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
+"""
+0. Pascal's Triangle
+"""
+
+
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-    
-    # Initialize the triangle with the first row
-    triangle = [[1]]
-    
-    for i in range(1, n):
-        # Get the previous row
-        prev_row = triangle[-1]
-        # Start the new row with a 1
-        new_row = [1]
-        
-        # Compute the values inside the row
-        for j in range(1, i):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-        
-        # End the row with a 1
-        new_row.append(1)
-        # Add the new row to the triangle
-        triangle.append(new_row)
-    
-    return triangle
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
